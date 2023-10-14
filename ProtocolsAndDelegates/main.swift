8c521f2173c989db5fa4c33bb6ebdb10fd6a56cc
Eren Pekdemir <93570359+erenlifetime@users.noreply.github.com>
2023-10-14 21:28:18 +0300

    main.swift

--- ProtocolsAndDelegates/main.swift
+++ ProtocolsAndDelegates/main.swift
@@ -1,11 +1,46 @@
-//
-//  main.swift
-//  ProtocolsAndDelegates
-//
-//  Created by Eren lifetime on 10.10.2023.
-//
-
-import Foundation
-
-print("Hello, World!")
-
+protocol AdvancedLifeSupport{
+    func performPCR()
+}
+class EmergencyCallHander {
+    var delegate:AdvancedLifeSupport?
+    func assesSituation(){
+        print("Söylermisin Ne Oldu?")
+    }
+    func medicalEmergency(){
+        delegate?.performPCR()
+    }
+    // ------
+    }
+struct Parademic: AdvancedLifeSupport{
+    func performPCR() {
+        
+    }
+    
+//    func performPCR() {
+//    }
+//    
+    init(handler:EmergencyCallHander) {
+        handler.delegate = self
+    }
+    class Doctor:AdvancedLifeSupport{
+        func useStethescope() {
+            print("Kalp Sesini Dinliyorum"   )
+        }
+        func performPCR() {
+            print("Paramedic saniyede 30 göğüs kompresyonu yapıyor ")
+        }
+    }
+    class Surgeon:Doctor{
+        override func performPCR() {
+            super.performPCR()
+            print("Hasta Ölümün Eşiğinde")
+        }
+    }
+//        init handler:EmergencyCallHander{
+//            handler.delegate = self
+}
+    func useElectricDrill() {
+    print("")
+    }
+let eren = EmergencyCallHander()
+let pete = Parademic(handler: eren)
